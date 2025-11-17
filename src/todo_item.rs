6cf9b6d16ee8b83d655bd271c8a6ca71eec5d51c
K1K000 <159767670+K1K000@users.noreply.github.com>
2025-11-17 09:54:12 +0100

    commit

--- src/todo_item.rs
+++ src/todo_item.rs
@@ -4,28 +4,31 @@ use rocket::serde::json::Json;
 use rocket::*;
 use sea_orm::*;
 
+// routes/todo_item/mod.rs -> fn add_routes(r: Rocket<Build>) -> Rocket<Build>
+// routes/todo_item/get.rs or something
+
 //todo proper routing cause this is ass
 #[get("/todoitem")]
 pub async fn get_todo_items(
     db: &State<DatabaseConnection>,
-) -> Result<Json<Vec<String>>, ErrorResponder> {
-    let db = db as &DatabaseConnection;
+) -> Result<Json<Vec<sea_orm::JsonValue>>, ErrorResponder> {
+    // type ORM = TodoItemGetORM;
+    let db = db.inner();
 
     let todo_items = TodoItem::find()
+        .into_json()
         .all(db)
-        .await
-        .unwrap()
-        .into_iter()
-        .map(|b| b.name)
-        .collect::<Vec<String>>();
+        // .map(|val| ORM::from(val)) // GetORM should have Serialize implemented (derived)
+        .await?;
     Ok(Json(todo_items))
 }
 
-// #[get("/todoitme/<id>")]
-// pub async fn get_todo_item_id(
-//     db: &State<DatabaseConnection>,
-//     id: i64,
-// ) -> Result<Json<String>, ErrorResponder> {
-//     let db = db as &DatabaseConnection;
-//
-// }
+#[get("/todoitem/<id>")]
+pub async fn get_todo_item_id(
+    db: &State<DatabaseConnection>,
+    id: i32,
+) -> Result<Json<Option<sea_orm::JsonValue>>, ErrorResponder> {
+    let db = db.inner();
+    let todo_item = TodoItem::find_by_id(id).into_json().one(db).await?;
+    Ok(Json(todo_item))
+}
