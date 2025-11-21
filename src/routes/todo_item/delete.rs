69b6a9c66efd6a33502ce854e80e22dc7b6ebcbc
K1K000 <159767670+K1K000@users.noreply.github.com>
2025-11-21 11:09:25 +0100

    put prolly works and now delete does

---
+++ src/routes/todo_item/delete.rs
@@ -0,0 +1,23 @@
+use crate::entities::prelude::TodoItem;
+use crate::errorhand::ErrorResponder;
+use rocket::http::Status;
+use rocket::*;
+use sea_orm::{entity::*, *};
+
+#[delete("/<id>")]
+pub async fn by_id(db: &State<DatabaseConnection>, id: i32) -> Result<Status, ErrorResponder> {
+    let db = db.inner();
+    // this is the better way but i dont get the sea_orm::DeleteResult type :(
+
+    // match TodoItem::delete_by_id(id).exec(db).await? {
+    //     Some(_) => Ok(Status::NoContent),
+    //     None => Err(ErrorResponder::NotFound(())),
+    // }
+    match TodoItem::find_by_id(id).one(db).await? {
+        Some(val) => {
+            val.delete(db).await?;
+            Ok(Status::NoContent)
+        }
+        None => Err(ErrorResponder::NotFound(())),
+    }
+}
--- src/routes/todo_item/mod.rs
+++ src/routes/todo_item/mod.rs
@@ -2,6 +2,7 @@ use rocket::{Build, Rocket, routes};
 
 use crate::mounter::Mounter;
 
+pub mod delete;
 pub mod get;
 pub mod post;
 pub mod put;
@@ -11,6 +12,9 @@ pub struct TodoItemMounter;
 
 impl Mounter for TodoItemMounter {
     fn mount(r: Rocket<Build>) -> Rocket<Build> {
-        r.mount("/todoitem", routes![get::all, get::by_id, post::single])
+        r.mount(
+            "/todoitem",
+            routes![get::all, get::by_id, post::single, put::put, delete::by_id],
+        )
     }
 }
